8d72293f97a9db2fd4fc55824273195addd209cd
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 19:09:48 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,99 +2,81 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Index() {
-	
   return (
     <>
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
-      <img width="500" src="/img/logo.png" />
-      </div>
+          <img width="500" src="/img/logo.png" alt="Logo" />
+        </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           Ordu Yönetimi
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       Ordu Yönetimi ile kendi sunucunuzu yönetmeye ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi Sunucu Yönetiminizi oluşturun, bu keyfi en iyi komutlarla yaşayın.
-	  </p>
+          Ordu Yönetimi ile kendi sunucunuzu yönetmeye ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi Sunucu Yönetiminizi oluşturun, bu keyfi en iyi komutlarla yaşayın.
+        </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
-  
-            <Link href={"https://discord.com/oauth2/authorize?client_id=1240735854511783936&permissions=8&integration_type=0&scope=bot+applications.commands"}>
-              <a
-                className={
-                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
-                }
-              >
-          Botu Sunucunuza Ekleyin
-              </a>
-            </Link>
-  <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Ordu Yönetimine Oy Ver</a>
-  </Link>
+          <Link href="https://discord.com/oauth2/authorize?client_id=1240735854511783936&permissions=8&integration_type=0&scope=bot+applications.commands">
+            <a className="flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white">
+              Botu Sunucunuza Ekleyin
+            </a>
+          </Link>
+          <div className="py-10"></div>
+          <Link href="https://top.gg/bot/774043716797071371/vote">
+            <a className="px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white">
+              Ordu Yönetimine Oy Ver
+            </a>
+          </Link>
         </div>
 
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
-        <Image width="600" height="600" src="/img/gift-left.png"  />
+          <Image width={600} height={600} src="/img/gift-left.png" alt="Gift Left" />
         </div>
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
-        <Image  width="600" height="600" src="/img/gift-right.png"  />
+          <Image width={600} height={600} src="/img/gift-right.png" alt="Gift Right" />
         </div>
       </div>
 
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
+        <div className="col-span-4">
+          <p className="text-white text-4xl font-bold">Ordu Yönetimi</p>
+          <p className="text-white text-md font-medium text-gray-500/75">Ordu Yönetimini sunucuna eklemek ister misin?</p>
+          <a href="https://discord.com/oauth2/authorize?client_id=1240735854511783936&permissions=8&integration_type=0&scope=bot+applications.commands" className="mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white">
+            <i className="fab fa-discord mr-2" /> Botu Sunucunuza Ekleyin
+          </a>
+        </div>
 
-
-     <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
-          <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Ordu Yönetimi" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Ordu Yönetimini sunucuna eklemek ister misin?" }}/>
-            <a href="https://discord.com/oauth2/authorize?client_id=1240735854511783936&permissions=8&integration_type=0&scope=bot+applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Botu Sunucunuza Ekleyin
-            </a>
+        <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <div>
+            <div className="text-white">
+              <p className="text-xl font-semibold mt-5"><i className="fa fa-cogs text-2xl text-amber-500" /> Optimizasyon</p>
+              <p className="text-gray-500 line-clamp-4">Sunucunuza özel hızlı ve kişiselleştirilebilir ayarlar ile Ordu Yönetimi size sunucu yönetme keyfini doyasıya yaşatıyor.</p>
+            </div>
           </div>
-
-          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
-           
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimizasyon</p>
-                  <p className="text-gray-500 line-clamp-4">Sunucunuza özel hızlı ve kişiselleştirilebilir ayarlar ile Ordu Yönetimi size sunucu yönetme keyfini doyasıya yaşatıyor.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Evrensel</p>
-                  <p className="text-gray-500 line-clamp-4">Tüm sunuculara uygun komutlara sahip bir bot.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Güvenlik</p>
-                  <p className="text-gray-500 line-clamp-4">Göz atabileceğiniz ve geliştirmemize yardımcı olabileceğiniz açık kaynaklı güvenli bir bot.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Destek</p>
-                  <p className="text-gray-500 line-clamp-4">En iyi ekip arkadaşlarımızla, yardıma ihtiyacı olan herkese en hızlı şekilde geri dönüş sağlıyoruz.</p>
-                </div>
-              </div>
+          <div>
+            <div className="text-white">
+              <p className="text-xl font-semibold mt-5"><i className="fa fa-universal-access text-2xl text-amber-500" /> Evrensel</p>
+              <p className="text-gray-500 line-clamp-4">Tüm sunuculara uygun komutlara sahip bir bot.</p>
+            </div>
           </div>
+          <div>
+            <div className="text-white">
+              <p className="text-xl font-semibold mt-5"><i className="fa fa-shield text-2xl text-amber-500" /> Güvenlik</p>
+              <p className="text-gray-500 line-clamp-4">Göz atabileceğiniz ve geliştirmemize yardımcı olabileceğiniz açık kaynaklı güvenli bir bot.</p>
+            </div>
+          </div>
+          <div>
+            <div className="text-white">
+              <p className="text-xl font-semibold mt-5"><i className="fa fa-ticket text-2xl text-amber-500" /> Destek</p>
+              <p className="text-gray-500 line-clamp-4">En iyi ekip arkadaşlarımızla, yardıma ihtiyacı olan herkese en hızlı şekilde geri dönüş sağlıyoruz.</p>
+            </div>
+          </div>
+        </div>
       </div>
-
-
 
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
-              <>
-                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Ordu Yönetimi Reflex</p>
-                  <p className="En iyi ve en güncel bot olan Ordu Yönetimi ile en iyi komutları kullanmaya hazır mısınız?</p>
-                </div>
-                <img src="https://cdn.discordapp.com/attachments/1275831480915857461/1276915983088615477/logo.png?ex=66cb442e&is=66c9f2ae&hm=eb543404a9c7ab4d8239168aebe918fbce7fd1e6158c65157b5accecd1762ff8&" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
-              </>
-      
+        <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+          <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png" width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" alt="Reflex" />
+          <p className="text-3xl text-white font-semibold">Ordu Yönetimi Reflex</p>
+          <p>En iyi ve en güncel bot olan Ordu Yönetimi ile en iyi komutları kullanmaya hazır mısınız?</p>
         </div>
-
-      </>
-  )
-}
+        <img src="https://cdn.discordapp.com/attachments/1275831480915857461/1276915983088615477/logo.png?ex=66cb442e&is=66c9f2ae&hm=eb543404a9c7ab4d8239168aebe918fbce7fd1e6158c651
