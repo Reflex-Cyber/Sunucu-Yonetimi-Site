d0c9121eb2c5706df626c5288df00e2c918f9018
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 21:11:33 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,15 +5,15 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Ordu Yönetimi Gizlilik Politikası</h1>
             <p className="text-sm text-white/30 mb-4">Son Güncelleme: 24/08/2024</p>
             <div className="animateHeader text-white text-opacity-40 mt-5">
-                <p>⊳ Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MESAJLARI OKUMA, MESAJ GÖNDERME, BAĞLANTILARI GÖMME, DOSYALARI EKLEME, MESAJ GEÇMİŞİNİ OKUMA, ÜYELERİ ENGELLE, ÜYELERİ AT, ÜYELERİ SUSTUR, BAĞLAN, MESAJLARI YÖNET, KANALLARI YÖNET, ROLLERİ YÖNET YADA DİREK YÖNETİCİ VEREBİLİRSİNİZ.)</a></p>
+                <p>Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MESAJLARI OKUMA, MESAJ GÖNDERME, BAĞLANTILARI GÖMME, DOSYALARI EKLEME, MESAJ GEÇMİŞİNİ OKUMA, ÜYELERİ ENGELLE, ÜYELERİ AT, ÜYELERİ SUSTUR, BAĞLAN, MESAJLARI YÖNET, KANALLARI YÖNET, ROLLERİ YÖNET YADA DİREK YÖNETİCİ VEREBİLİRSİNİZ.)</a></p>
                 <div className="py-3"></div>
-                <p>⊳ Her kullanıcı, Ordu Yönetimini botunu zararlı amaçlar gütmeden dilediği gibi kullanabilir.</p>
+                <p>Her kullanıcı, Ordu Yönetimini botunu zararlı amaçlar gütmeden dilediği gibi kullanabilir.</p>
                 <div className="py-3"></div>
-                <p>⊳ Ordu Yönetimini botunu kullanırken bazı komutları aynı anda kullanamazsınız.</p>
+                <p>Ordu Yönetimini botunu kullanırken bazı komutları aynı anda kullanamazsınız.</p>
                 <div className="py-3"></div>
-                <p>⊳ Ordu Yönetimi botu, kullanıcılarla ilgili herhangi bir veri saklamaz.</p>
+                <p>Ordu Yönetimi botu, kullanıcılarla ilgili herhangi bir veri saklamaz.</p>
                 <div className="py-3"></div>
-                <p>⊳ Botu kullanırken, kullanıcı ID'niz sistemde yalnızca 60 saniye boyunca kaydedilir ve ardından silinir.</p>
+                <p>Botu kullanırken, kullanıcı ID'niz sistemde yalnızca 60 saniye boyunca kaydedilir ve ardından silinir.</p>
             </div>
         </div>
     );
