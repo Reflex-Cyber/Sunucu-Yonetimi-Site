207a30fc08cf3db9be54c1ee7ae4825e636e23e7
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 17:27:42 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,8 +26,11 @@ export default function Index() {
           Botu Sunucunuza Ekleyin
               </a>
             </Link>
-<div className="py-10"></div>
-
+  <div className="py-10"></div>
+  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
+  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
+Ordu Yönetimine Oy Ver</a>
+  </Link>
         </div>
 
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
@@ -37,8 +40,10 @@ export default function Index() {
         <Image  width="600" height="600" src="/img/gift-right.png"  />
         </div>
       </div>
-	    
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
+
+
+
+     <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Ordu Yönetimi" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Ordu Yönetimini sunucuna eklemek ister misin?" }}/>
