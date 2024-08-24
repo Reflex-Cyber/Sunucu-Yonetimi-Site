40ee9e74eb9d0e09ac5ec316028a304732d77c27
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 21:08:19 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,9 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Ordu Yönetimi Gizlilik Politikası</h1>
             <p className="text-sm text-white/30 mb-4">Son Güncelleme: 24/08/2024</p>
             <div className="animateHeader text-white text-opacity-40 mt-5">
-                <p>⊳ Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MESAJLARI OKUMA, MESAJ GÖNDERME, BAĞLANTILARI GÖMME, DOSYALARI EKLEME, MESAJ GEÇMİŞİNİ OKUMA, BAĞLAN, KONUŞ, SES AKTİVİTESİ KULLAN, ÖNCELİKLİ KONUŞMACI)</a></p>
-                <div className="py-3"></div>
-                <p>⊳ Bot komutlarından bazılarını kullanırken <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> botuna oy vermeniz gerekmektedir.</p>
+                <p>⊳ Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MESAJLARI OKUMA, MESAJ GÖNDERME, BAĞLANTILARI GÖMME, DOSYALARI EKLEME, MESAJ GEÇMİŞİNİ OKUMA, ÜYELERİ ENGELLE, ÜYELERİ AT, ÜYELERİ SUSTUR, BAĞLAN, MESAJLARI YÖNET, KANALLARI YÖNET, ROLLERİ YÖNET YADA DİREK YÖNETİCİ VEREBİLİRSİNİZ.)</a></p>
                 <div className="py-3"></div>
                 <p>⊳ Her kullanıcı, Ordu Yönetimini botunu zararlı amaçlar gütmeden dilediği gibi kullanabilir.</p>
                 <div className="py-3"></div>
