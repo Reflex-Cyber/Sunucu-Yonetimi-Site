f792f9e1dc5323332163b92b207fd2f97dc388b8
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 21:12:46 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,8 +2,8 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Ordu Yönetimi Gizlilik Politikası</h1>
-            <p className="text-sm text-white/30 mb-4">Son Güncelleme: 24/08/2024</p>
+      <h2 className="mt-4 text-white font-bold text-3xl">Ordu Yönetimi Gizlilik Politikası</h2>
+      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 24/08/2024</p>
             <div className="animateHeader text-white text-opacity-40 mt-5">
                 <p>Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MESAJLARI OKUMA, MESAJ GÖNDERME, BAĞLANTILARI GÖMME, DOSYALARI EKLEME, MESAJ GEÇMİŞİNİ OKUMA, ÜYELERİ ENGELLE, ÜYELERİ AT, ÜYELERİ SUSTUR, BAĞLAN, MESAJLARI YÖNET, KANALLARI YÖNET, ROLLERİ YÖNET YADA DİREK YÖNETİCİ VEREBİLİRSİNİZ.)</a></p>
                 <div className="py-3"></div>
