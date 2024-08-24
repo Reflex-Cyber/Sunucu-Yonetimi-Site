34038077fe4633a905d605701e6912afff1b5ca2
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 21:04:37 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,22 +2,21 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
-  <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
-        <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
-        <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
-        <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
-        <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
-        <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
-    
-        </div>
+            <h1 className="font-semibold text-3xl text-white/75">Ordu Yönetimi Gizlilik Politikası</h1>
+            <p className="text-sm text-white/30 mb-4">Son Güncelleme: 24/08/2024</p>
+            <div className="animateHeader text-white text-opacity-40 mt-5">
+                <p>⊳ Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(MESAJLARI OKUMA, MESAJ GÖNDERME, BAĞLANTILARI GÖMME, DOSYALARI EKLEME, MESAJ GEÇMİŞİNİ OKUMA, BAĞLAN, KONUŞ, SES AKTİVİTESİ KULLAN, ÖNCELİKLİ KONUŞMACI)</a></p>
+                <div className="py-3"></div>
+                <p>⊳ Bot komutlarından bazılarını kullanırken <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> botuna oy vermeniz gerekmektedir.</p>
+                <div className="py-3"></div>
+                <p>⊳ Her kullanıcı, Ordu Yönetimini botunu zararlı amaçlar gütmeden dilediği gibi kullanabilir.</p>
+                <div className="py-3"></div>
+                <p>⊳ Ordu Yönetimini botunu kullanırken bazı komutları aynı anda kullanamazsınız.</p>
+                <div className="py-3"></div>
+                <p>⊳ Ordu Yönetimi botu, kullanıcılarla ilgili herhangi bir veri saklamaz.</p>
+                <div className="py-3"></div>
+                <p>⊳ Botu kullanırken, kullanıcı ID'niz sistemde yalnızca 60 saniye boyunca kaydedilir ve ardından silinir.</p>
             </div>
+        </div>
     );
 };
