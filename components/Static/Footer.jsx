3983562ff6eb205cb9631c49f398b267589e28d0
Reflexbro12 <yusufkutluay526@gmail.com>
2024-08-24 17:37:33 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -58,21 +58,7 @@ export default function Footer() {
                             <div>
                                 <Link href="https://discord.gg/p9WDE4QSyB">
                                     <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-discord`} /> Disocrd 
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href="https://github.com/umutxyp/MusicBot">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-github`} /> GitHub 
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href="https://youtube.com/UmutBayraktarYT">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-youtube`} /> YouTube 
+                                    <i className={`fa-brands fa-discord`} /> Discord 
                                     </a>
                                 </Link>
                             </div>
