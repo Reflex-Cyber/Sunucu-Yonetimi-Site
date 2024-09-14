10d0a428ea4dc5e18652a75b3c4c9937cc28c38e
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 17:25:33 +0300

    Update hizmet-sartlari.jsx

--- pages/hizmet-sartlari.jsx
+++ pages/hizmet-sartlari.jsx
@@ -2,7 +2,7 @@ export default function Tos() {
   return (
     <div>
       <h2 className="mt-4 text-white font-bold text-3xl">Sunucu Yönetimi Hizmet Şartları</h2>
-      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 24/08/2024</p>
+      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 14/09/2024</p>
       <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>Bütün Discord sunucuları, Sunucu Yönetimi botunu ücretsiz olarak kullanabilir, botu satmak veya ücretli olarak pazarlamak tamamen yasaktır.</p>
         <div className="py-3"></div>
