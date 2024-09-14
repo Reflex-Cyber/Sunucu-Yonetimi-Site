fa0e1a453217b2df86bebb63d1abe5840006621a
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 16:19:52 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -48,6 +48,24 @@ export default function Footer() {
                             </div>
                         </div>
                     </div>
+                        <div className="col-span-1">
+                            <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Important</p>
+                            <div>
+                                <Link href="/gizlilik-şartları">
+                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
+                                        Gizlilik Şartları
+                                    </a>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link href="/hizmet-şartları">
+                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
+                                        Hizmet Şartları
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <p className="text-white text-center sm:text-left text-opacity-50">
                             {new Date().getFullYear()} &copy; Sunucu Yönetimi
