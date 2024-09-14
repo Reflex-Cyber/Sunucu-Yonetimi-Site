806f0d8f088af4a40c97ebc84284eb593df14281
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 11:32:59 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function Index() {
     <>
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
-      <img width="500" src="https://www.freepnglogos.com/uploads/ataturk-png/mustafa-kemal-ataturk-imzasi-png-photo-8.png" />
+      <img width="500" src="/img/logo.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           Sunucu Yönetimi
@@ -46,13 +46,13 @@ export default function Index() {
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Evrensel</p>
-                  <p className="text-gray-500 line-clamp-4">Tüm sunuculara göre daha farklı ve daha özel bir sunucu.</p>
+                  <p className="text-gray-500 line-clamp-4">Tüm botlara göre daha farklı ve daha özel bir bot.</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Güvenlik</p>
-                  <p className="text-gray-500 line-clamp-4">Göz atabileceğiniz ve geliştirmemize yardımcı olabileceğiniz açık arada güvenli bir sunucu.</p>
+                  <p className="text-gray-500 line-clamp-4">Göz atabileceğiniz ve geliştirmemize yardımcı olabileceğiniz açık arada güvenli bir bot.</p>
                 </div>
               </div>
               <div>
@@ -70,10 +70,10 @@ export default function Index() {
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Eski TA - TPT</p>
+                  <p className="text-3xl text-white font-semibold">Sunucu Yönetimi</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel sunucu olan Eski TA - TPT ile en iyi eğlenceyi yaşamaya hazır mısınız?</p>
                 </div>
-                <img src="https://avatars.mds.yandex.net/i?id=0f31946f21346c220db0e19e91ef0c35ebac00bb-4593296-images-thumbs&n=13" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="/img/logo.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
