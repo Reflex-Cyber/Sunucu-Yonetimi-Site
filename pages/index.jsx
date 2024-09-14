99eaeced7e568027436d2418f558e2303e4864b1
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 11:14:21 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,10 +10,10 @@ export default function Index() {
       <img width="500" src="https://www.freepnglogos.com/uploads/ataturk-png/mustafa-kemal-ataturk-imzasi-png-photo-8.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Eski TA - TPT
+          Sunucu Yönetimi
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       Eski TA - TPT ile eğlencenizi iki katına çıkarmaya ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi eğlencenizi oluşturun, bu keyfi en iyi arkadaşlarla yaşayın. 
+       Sunucu Yönetimi ile eğlencenizi iki katına çıkarmaya ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi sunucunuzu oluşturun, bu keyfi en iyi komutlarla yaşayın. 
 	  </p>
 
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
@@ -28,9 +28,9 @@ export default function Index() {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Eski TA - TPT" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Eski TA ve TPT sunucusunda keyfini iki katına çıkarmak ister misin?" }}/>
-            <a href="https://discord.gg/9TndYERNqE" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Sunucu Yönetimi" }} />
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Sunucu Yönetimi sunucusunda keyfini iki katına çıkarmak ister misin?" }}/>
+            <a href="https://discord.gg/dAFnKjYhcf" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
               <i className="fab fa-discord mr-2" />Sunucumuza Katıl!
             </a>
           </div>
