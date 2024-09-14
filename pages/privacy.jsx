ba0e10b20cf1b8f9bb4845d4030290513d5cd833
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 11:22:54 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,16 +2,16 @@ export default function Privacy() {
 
     return (
         <div>
-      <h2 className="mt-4 text-white font-bold text-3xl">Ordu Yönetimi Gizlilik Politikası</h2>
+      <h2 className="mt-4 text-white font-bold text-3xl">Sunucu Yönetimi Gizlilik Politikası</h2>
       <p className="text-sm text-white/30 mb-1">Son Güncelleme: 24/08/2024</p>
             <div className="animateHeader text-white text-opacity-40 mt-5">
-                <p>Ordu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(Bu tanımlamanız gereken izin ise Yönetici yetkisi izinidir.)</a></p>
+                <p>Sunucu Yönetimi botunu kullanmak için, botu Discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(Bu tanımlamanız gereken izin ise Yönetici yetkisi izinidir.)</a></p>
                 <div className="py-3"></div>
-                <p>Her kullanıcı, Ordu Yönetimini botunu zararlı amaçlar gütmeden dilediği gibi kullanabilir.</p>
+                <p>Her kullanıcı, Sunucu Yönetimini botunu zararlı amaçlar gütmeden dilediği gibi kullanabilir.</p>
                 <div className="py-3"></div>
-                <p>Ordu Yönetimini botunu kullanırken bazı komutları aynı anda kullanamazsınız.</p>
+                <p>Sunucu Yönetimini botunu kullanırken bazı komutları aynı anda kullanamazsınız.</p>
                 <div className="py-3"></div>
-                <p>Ordu Yönetimi botu, kullanıcılarla ilgili herhangi bir veri saklamaz.</p>
+                <p>Sunucu Yönetimi botu, kullanıcılarla ilgili herhangi bir veri saklamaz.</p>
                 <div className="py-3"></div>
                 <p>Botu kullanırken, kullanıcı ID'niz sistemde yalnızca 60 saniye boyunca kaydedilir ve ardından silinir.</p>
             </div>
