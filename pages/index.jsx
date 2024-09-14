4bc4e30936d3f09e1ea8c70b14fe71791441b0dc
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 16:57:53 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,7 +23,19 @@ export default function Index() {
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Botu Sunucunuza Ekleyiniz
+          Botu Sunucuna Ekle
+              </a>
+            </Link>
+	 </div>
+        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
+  
+            <Link href={"https://discord.gg/dAFnKjYhcf"}>
+              <a
+                className={
+                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
+                }
+              >
+          Destek Sunucusu
               </a>
             </Link>
 	 </div>
