5a01507d661372653f565e1313437d985ecbd215
Reflexbro12 <yusufkutluay526@gmail.com>
2024-09-14 11:23:25 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,10 +1,10 @@
 export default function Tos() {
   return (
     <div>
-      <h2 className="mt-4 text-white font-bold text-3xl">Ordu Yönetimi Hizmet Şartları</h2>
+      <h2 className="mt-4 text-white font-bold text-3xl">Sunucu Yönetimi Hizmet Şartları</h2>
       <p className="text-sm text-white/30 mb-1">Son Güncelleme: 24/08/2024</p>
       <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>Her Discord sunucusu Ordu Yönetimi botunu ücretsiz olarak kullanabilir, botu satmak veya ücretli olarak pazarlamak tamamen yasaktır.</p>
+        <p>Her Discord sunucusu Sunucu Yönetimi botunu ücretsiz olarak kullanabilir, botu satmak veya ücretli olarak pazarlamak tamamen yasaktır.</p>
         <div className="py-3"></div>
         <p>Bot komutlarını botu zarar vermek amacıyla kullanamazsınız.</p>
         <div className="py-3"></div>
